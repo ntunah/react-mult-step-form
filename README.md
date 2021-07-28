7208225dd31cddc8c308d8990bc1df31b1e6b56d
ntunah <ntuthukomadondo777@gmail.com>
2021-07-28 03:35:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-buwpgp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-buwpgp)
---
+++ src/router/AppRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import FirstStep from '../components/steps/FirstStep';
+
+const AppRouter = () => (
+  <BrowserRouter>
+<div className="container">
+<Header />
+    <Switch>
+      <Route component={FirstStep} path="/" exact={true} />
+    </Switch>
+</div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
